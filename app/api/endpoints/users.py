24e777c2f716c48fe66143c4cc094823ec344a8b
SnowSuno <snowsuno.dev@gmail.com>
2022-01-05 17:37:52 +0900

    feat : todo crud

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends
+from typing import List
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, HTTPException
 from app import schemas, models
 from app.core.dependancies import get_current_user
 
@@ -9,5 +12,52 @@ router = APIRouter(
 
 @router.get("/me", response_model=schemas.User)
 async def read_current_user(current_user: models.User = Depends(get_current_user)):
-    print(current_user.hashed_password)
     return current_user
+
+
+@router.get("/me/todos", response_model=List[schemas.Todo])
+async def read_todos_of_current_user(current_user: models.User = Depends(get_current_user)):
+    return await current_user.todos
+
+@router.post("/me/todos", response_model=schemas.Todo, status_code=201)
+async def create_todo(
+    todo: schemas.TodoCreate,
+    current_user: models.User = Depends(get_current_user)
+):
+    return await models.Todo.create(**todo.dict(), creator_id=current_user.uuid)
+
+async def get_updateable_todo(
+    todo_id: UUID,
+    current_user: models.User = Depends(get_current_user)
+) -> models.Todo:
+    todo = await models.Todo.get(id=todo_id)
+    if todo is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Todo not found"
+        )
+
+    creator = await todo.creator
+    if creator.uuid != current_user.uuid:
+        raise HTTPException(
+            status_code=403,
+            detail="Can only update todos created by user"
+        )
+    return todo
+
+@router.put("/me/todos/{todo_id}", response_model=schemas.Todo)
+async def update_todo(
+    todo: schemas.TodoUpdate,
+    prev_todo: models.Todo = Depends(get_updateable_todo)
+):
+    await prev_todo.update_from_dict(
+        todo.dict(exclude_unset=True)
+    )
+    await prev_todo.save()
+    return prev_todo
+
+@router.delete("/me/todos/{todo_id}", response_model=None, status_code=204)
+async def update_todo(
+    prev_todo: models.Todo = Depends(get_updateable_todo)
+):
+    await prev_todo.delete()
--- app/models/user_items.py
+++ app/models/user_items.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from tortoise import fields, models
+
 
 class User(models.Model):
     uuid = fields.UUIDField(pk=True)
@@ -9,5 +11,19 @@ class User(models.Model):
     is_verified = fields.BooleanField(default=False)
     is_active = fields.BooleanField(default=False)
     registered_at = fields.DatetimeField(auto_now=True)
+
+    todos: fields.ReverseRelation[Todo]
+
+
+class Todo(models.Model):
+    id = fields.UUIDField(pk=True)
+    title = fields.CharField(max_length=200)
+    is_done = fields.BooleanField(default=False)
+
+    creator: fields.ForeignKeyRelation[User] = fields.ForeignKeyField(
+        "models.User", related_name="todos"
+    )
+
+
 
 
