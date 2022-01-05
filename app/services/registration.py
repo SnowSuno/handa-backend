8a56601d236ec63d49f19337e0e1890bd7bb9239
SnowSuno <snowsuno.dev@gmail.com>
2022-01-06 01:28:57 +0900

    fix : rename id to username

--- app/services/registration.py
+++ app/services/registration.py
@@ -1,8 +1,8 @@
 from fastapi import HTTPException
 from app import schemas, models
 
-async def check_id_is_available(user_id: str, auto_exception: bool = False) -> bool:
-    is_available = not bool(await models.User.get_or_none(id=user_id))
+async def check_username_is_available(username: str, auto_exception: bool = False) -> bool:
+    is_available = not bool(await models.User.get_or_none(username=username))
     if auto_exception and not is_available:
         raise HTTPException(
             status_code=400,
@@ -20,6 +20,6 @@ async def check_email_is_available(email: str, auto_exception: bool = False) -> 
     return is_available
 
 async def check_unique_fields_is_available(user: schemas.UserCreate, auto_exception: bool = False):
-    id_is_available = await check_id_is_available(user.username, auto_exception=auto_exception)
+    username_is_available = await check_username_is_available(user.username, auto_exception=auto_exception)
     email_is_available = await check_email_is_available(user.email, auto_exception=auto_exception)
-    return id_is_available and email_is_available
+    return username_is_available and email_is_available
