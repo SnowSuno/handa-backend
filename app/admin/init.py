b2079fa9610511cfa4235ab4307d3afbb68819fc
SnowSuno <snowsuno.dev@gmail.com>
2022-01-08 21:58:09 +0900

    feat : foreign key handling in admin page

--- app/admin/init.py
+++ app/admin/init.py
@@ -1,8 +1,11 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from starlette import status
 from fastapi_admin.app import app as admin_app
 from fastapi_admin.providers.login import UsernamePasswordProvider
+from fastapi_admin import exceptions
 from fakeredis import aioredis
 
 from app import models
@@ -15,6 +18,14 @@ login_provider = UsernamePasswordProvider(
 )
 
 def admin_init(app: FastAPI):
+    admin_app.add_exception_handler(status.HTTP_500_INTERNAL_SERVER_ERROR, exceptions.server_error_exception)
+    admin_app.add_exception_handler(status.HTTP_404_NOT_FOUND, exceptions.not_found_error_exception)
+    admin_app.add_exception_handler(status.HTTP_403_FORBIDDEN, exceptions.forbidden_error_exception)
+    admin_app.add_exception_handler(
+        status.HTTP_401_UNAUTHORIZED,
+        lambda *args: RedirectResponse("/admin/login")
+    )
+
     @app.on_event("startup")
     async def startup():
         redis = aioredis.FakeRedis()
--- app/admin/resources.py
+++ app/admin/resources.py
@@ -1,11 +1,11 @@
 
 from fastapi_admin.app import app
-from fastapi_admin.resources import Action, Dropdown, Field, Link, Model, ToolbarAction
+from fastapi_admin.resources import Field, Link, Model, ComputeField
 from fastapi_admin.widgets import displays, filters, inputs
 
 from app import models
 
-from . import widgets as custom
+from . import custom
 
 
 @app.register
@@ -48,7 +48,14 @@ class UserResource(Model):
     icon = "fas fa-user"
     page_pre_title = "user list"
     page_title = "user model"
-
+    filters = [
+        filters.Search(
+            name="username",
+            label="username",
+            search_mode="contains",
+            placeholder="search for an username"
+        )
+    ]
     fields = [
         Field(
             name="id",
@@ -59,6 +66,12 @@ class UserResource(Model):
         "username",
         "email",
         "nickname",
+        # custom.fields.HashedPasswordField(
+        #     name="hashed_password",
+        #     label="password",
+        #     display=displays.InputOnly(),
+        #     input_=inputs.Password()
+        # ),
         Field(
             name="hashed_password",
             label="password",
@@ -72,8 +85,52 @@ class UserResource(Model):
 
 
 @app.register
-class GithubLink(Link):
-    label = "Github"
-    url = "https://google.com/"
-    icon = "fab fa-github"
+class TodoResource(Model):
+    label = "Todos"
+    model = models.Todo
+    icon = "fas fa-clipboard-list"
+    page_pre_title = "todo list"
+    page_title = "todo model"
+    filters = [
+        custom.filters.ForeignFieldSearch(
+            name="creator",
+            label="creator",
+            key_field="username",
+            placeholder="search by creator",
+            null=True
+        )
+    ]
+    fields = [
+        "id",
+        "title",
+        "is_done",
+        Field(
+            name="creator_id",
+            label="creator",
+            display=displays.InputOnly(),
+            # input_=inputs.Input(help_text="creator_id"),
+            input_=inputs.ForeignKey(
+                model=models.User,
+                null=False,
+                default=None
+            ),
+        ),
+        custom.fields.ForeignKeyDisplayField(
+            model=models.User,
+            name="creator_id",
+            label="creator",
+            display_field="username",
+            display=displays.Display(),
+            input_=inputs.DisplayOnly()
+        )
+
+
+    ]
+
+
+@app.register
+class DocumentationLink(Link):
+    label = "Documentation"
+    url = "https://service-handa.herokuapp.com/docs"
+    icon = "fas fa-file-code"
     target = "_blank"
