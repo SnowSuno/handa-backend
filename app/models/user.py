319f2ec3388d3bb0984ec7ff01debeabf9fe9ea2
SnowSuno <snowsuno.dev@gmail.com>
2022-01-12 04:59:10 +0900

    refactor : implement user-related methods to user model methods

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,11 @@
-from tortoise import fields, models
+from typing import TYPE_CHECKING, Optional
 
-from typing import TYPE_CHECKING
+from fastapi import HTTPException
+from tortoise import fields, models
+from email_validator import validate_email, EmailNotValidError
+
+from app import schemas
+from app.core.security import hash_password, pwd_context, create_access_token
 
 if TYPE_CHECKING:
     from .todo import Todo
@@ -19,3 +24,37 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+    @classmethod
+    async def register(cls, user: schemas.UserCreate):
+        return await cls.create(
+            **user.dict(),
+            hashed_password=hash_password(user.password)
+        )
+
+    @classmethod
+    async def authenticate(cls, username: str, password: str) -> schemas.Token:
+        user = await cls.get_by_username_or_email(username)
+        if not user:
+            raise HTTPException(
+                status_code=404,
+                detail="User with such email or id does not exist",
+            )
+        if not pwd_context.verify(password, user.hashed_password):
+            raise HTTPException(
+                status_code=401,
+                detail="Incorrect password"
+            )
+        token = create_access_token(str(user.id))
+        return schemas.Token(
+            access_token=token,
+            token_type="bearer"
+        )
+
+    @classmethod
+    async def get_by_username_or_email(cls, identifier: str) -> Optional["User"]:
+        try:
+            valid = validate_email(identifier)
+            return await cls.get(email=valid.email)
+        except EmailNotValidError:
+            return await cls.get_or_none(username=identifier)
