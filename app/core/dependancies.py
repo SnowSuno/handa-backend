44fcc6abf97f4782cfb8c0a8d61fa06268d3e691
SnowSuno <snowsuno.dev@gmail.com>
2022-01-20 17:40:15 +0900

    chore : pep lint

--- app/core/dependancies.py
+++ app/core/dependancies.py
@@ -8,6 +8,7 @@ oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="auth/login-docs"
 )
 
+
 async def get_current_user(token: str = Depends(oauth2_scheme)) -> models.User:
     credentials_exception = HTTPException(
         status_code=401,
