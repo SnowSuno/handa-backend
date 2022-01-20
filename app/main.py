30a216e7c6474d60f9c26cf9fe0185c150aedf85
SnowSuno <snowsuno.dev@gmail.com>
2022-01-20 16:09:45 +0900

    chore : remove blank lines

--- app/main.py
+++ app/main.py
@@ -4,12 +4,9 @@ from app.core.config import settings
 from app.admin.init import admin_init
 from app.db.database import db_init
 
-
-
 app = FastAPI(
     title=settings.PROJECT_NAME
 )
-
 
 app.include_router(api.router)
 
