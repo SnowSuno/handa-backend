803591c5197f79e81fa0b8edf0d0997f7f90fa0c
SnowSuno <snowsuno.dev@gmail.com>
2022-01-23 15:18:14 +0900

    fix : user check id fix

--- app/api/endpoints/todos.py
+++ app/api/endpoints/todos.py
@@ -9,9 +9,10 @@ router = APIRouter(
     }
 )
 
+
 async def get_updateable_todo(
-    todo_id: UUID,
-    current_user: models.User = Depends(get_current_user)
+        todo_id: UUID,
+        current_user: models.User = Depends(get_current_user)
 ) -> models.Todo:
     todo = await models.Todo.get(id=todo_id)
     if todo is None:
@@ -21,24 +22,26 @@ async def get_updateable_todo(
         )
 
     creator = await todo.creator
-    if creator.username != current_user.id:
+    if creator.id != current_user.id:
         raise HTTPException(
             status_code=403,
             detail="Can only update todos created by user"
         )
     return todo
 
+
 @router.post("/", response_model=schemas.Todo, status_code=201)
 async def create_todo(
-    todo: schemas.TodoCreate,
-    current_user: models.User = Depends(get_current_user)
+        todo: schemas.TodoCreate,
+        current_user: models.User = Depends(get_current_user)
 ):
     return await models.Todo.create(**todo.dict(), creator_id=current_user.id)
 
+
 @router.put("/{todo_id}", response_model=schemas.Todo)
 async def update_todo(
-    todo: schemas.TodoUpdate,
-    prev_todo: models.Todo = Depends(get_updateable_todo)
+        todo: schemas.TodoUpdate,
+        prev_todo: models.Todo = Depends(get_updateable_todo)
 ):
     await prev_todo.update_from_dict(
         todo.dict(exclude_unset=True)
@@ -46,8 +49,9 @@ async def update_todo(
     await prev_todo.save()
     return prev_todo
 
+
 @router.delete("/{todo_id}", response_model=None, status_code=204)
 async def update_todo(
-    prev_todo: models.Todo = Depends(get_updateable_todo)
+        prev_todo: models.Todo = Depends(get_updateable_todo)
 ):
     await prev_todo.delete()
